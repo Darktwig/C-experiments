1d6b6e9580144b5ffeac903190324e8b4df3aa52
Darktwig <cd /home/Baptiste/Documents/Programmation/C>
2026-03-20 20:27:37 +0100

    First commit

---
+++ Hello world.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello the world";
+    return 0;
+}
---
+++ Math Espressions.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int addition() {
+    int x=10;
+    int y=4;
+    int z=x+y;
+    std::cout <<z;
+    return 0;
+}
